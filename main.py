8db3453d9acff1b9a494cdfaaa0611947c4e0e15
Mustafa Mert Kara <mustafa.m.kara97@gmail.com>
2025-02-25 13:32:38 +0300

    added letter counting and sorting functions

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import os
-from stats import count_words
+import sys
+from stats import count_words,count_letters
 def get_book_text(path):
     with open(path) as fp:
         file_contents=fp.read()
     return file_contents
 
+def print_screen(path,wc,char_count):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {wc} total words")
+    print("--------- Character Count -------")
+    for val in char_count:
+        if val.isalpha():
+            print(f"{val}: {char_count[val]}")
+    print("============= END ===============")
+
 def main():
-    text=get_book_text("books/frankenstein.txt")
-    num_words=count_words(text)
-    print(f"{num_words} words found in the document")
+    try:
+        PATH=sys.argv[1]
+    except:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text=get_book_text(PATH)
+    num_words=count_words(text)    
+    letter_count=count_letters(text)
+    print_screen(PATH,num_words,letter_count)
+
 
 if __name__ == '__main__' :
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,28 @@
 def count_words(text):
     word_list=text.split()
     return len(word_list)
+
+def count_letters(text):
+    text=text.lower()
+    letter_count={}
+    for char in text:
+        try:
+            letter_count[char]+=1
+        except:
+            letter_count[char]=1
+    return sort_dict(letter_count)
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_dict(dict):
+    list_of_letters=[]
+    for val in dict:
+        list_of_letters.append({"letter":val,"count":dict[val]})
+    list_of_letters.sort(reverse=True,key=sort_on)
+    result_dict={}
+    for val in list_of_letters:
+        result_dict[val["letter"]]=val["count"]
+    return result_dict
+
+
